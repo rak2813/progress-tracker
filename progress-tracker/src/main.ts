53d9738b8341c7e445298e7063411e943539f670
rakshith-dasari <rakshith.dasari@swimlane.com>
2024-10-05 14:26:48 +0530

    main.ts

--- progress-tracker/src/main.ts
+++ progress-tracker/src/main.ts
@@ -5,11 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
-  app.enableCors({
-    origin: 'http://127.0.0.1:5500', // Replace with your frontend URL
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  // app.enableCors({
+  //   origin: 'http://127.0.0.1:5500', // Replace with your frontend URL
+  //   methods: 'GET,POST,PUT,DELETE',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
